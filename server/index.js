cbd8cab6e328a5c5d78b77793d1c110ea9b3d07f
VISHNU <55990388+hellno619@users.noreply.github.com>
2023-09-12 22:46:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,16 +4,26 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 import { userRoute } from "./routes/userRoute.js";
 import { residencyRoute } from "./routes/residencyRoute.js";
-dotenv.config()
+
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
-app.listen(PORT , ()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.use(express.json());
+app.use(cookieParser());
 
+// Configure CORS to allow requests from 'https://final-project-woad-omega.vercel.app'
+const corsOptions = {
+  origin: 'https://final-project-woad-omega.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Include cookies in cross-origin requests if needed
+};
+
+app.use(cors(corsOptions));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-app.use('/api/user' , userRoute)
-app.use("/api/residency" , residencyRoute)
+
+app.use('/api/user', userRoute);
+app.use("/api/residency", residencyRoute);
